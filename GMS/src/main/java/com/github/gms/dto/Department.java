9fe59e48535a9c5d096c5e0b6a00cc00f23ad0c1
briannaschladweiler <brischlads@gmail.com>
2020-03-11 13:30:33 -0500

    dao updates

--- GMS/src/main/java/com/github/gms/dto/Department.java
+++ GMS/src/main/java/com/github/gms/dto/Department.java
@@ -4,36 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
-* @author Ethan Bettenga
-* Department
-*/
-
+ * @author Ethan Bettenga Department
+ */
 public class Department {
-	String name;
-	List<Item> items = new ArrayList<>();
 
-	public Department(){
+    String name;
+    List<Item> items = new ArrayList<>();
 
-	}
+    public Department() {
 
-	public String getName() {
-		return this.name;
-	}
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Department(String name) {
+        this.name = name;
+    }
 
-	public List<Item> getItems() {
-		return this.items;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void addItem(Item item){
-		items.add(item);
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void removeItem(Item item){
-		items.remove(item);
-	}
-	
+    public List<Item> getItems() {
+        return this.items;
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
 }
