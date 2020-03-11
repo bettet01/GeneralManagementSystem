1d15605b9d5c0b5f7b531fb555f6cf3d0d078774
bettet01 <bettet01@luther.edu>
2020-03-11 10:26:56 -0500

    created Item dto

--- GMS/src/main/java/com/github/gms/dto/Item.java
+++ GMS/src/main/java/com/github/gms/dto/Item.java
@@ -5,10 +5,69 @@
  */
 package com.github.gms.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  *
  * @author briannaschladweiler
  */
 public class Item {
-    
+		String name;
+		LocalDate expDate;
+		int itemCount;
+		BigDecimal ppu;
+		String department;
+
+	public Item(){
+		
+	}
+
+	public Item(String name, LocalDate expDate, int itemCount, BigDecimal ppu, String department) {
+		this.name = name;
+		this.expDate = expDate;
+		this.itemCount = itemCount;
+		this.ppu = ppu;
+		this.department = department;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getExpDate() {
+		return this.expDate;
+	}
+
+	public void setExpDate(LocalDate expDate) {
+		this.expDate = expDate;
+	}
+
+	public int getItemCount() {
+		return this.itemCount;
+	}
+
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+
+	public BigDecimal getPpu() {
+		return this.ppu;
+	}
+
+	public void setPpu(BigDecimal ppu) {
+		this.ppu = ppu;
+	}
+
+	public String getDepartment() {
+		return this.department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 }
