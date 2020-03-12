2ca76023946a254262951b78a75319f8611afd23
Jake Curley <curleyjake@gmail.com>
2020-03-12 13:43:58 -0500

    spring

--- GMS/src/main/java/com/github/gms/App.java
+++ GMS/src/main/java/com/github/gms/App.java
@@ -8,6 +8,8 @@ import com.github.gms.service.ServiceLayerFileImpl;
 import com.github.gms.ui.GMSView;
 import com.github.gms.ui.UserIO;
 import com.github.gms.ui.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * GMS Main
@@ -15,11 +17,15 @@ import com.github.gms.ui.UserIOConsoleImpl;
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        UserIO io = new UserIOConsoleImpl();
-        GMSView view = new GMSView(io);
-        GmsDao dao = new GmsDaoFileImpl();
-        ServiceLayer service = new ServiceLayerFileImpl(dao);
-        GmsController controller = new GmsController(view, service);
-        controller.execute();
+        //UserIO io = new UserIOConsoleImpl();
+        //GMSView view = new GMSView(io);
+        //GmsDao dao = new GmsDaoFileImpl();
+        //ServiceLayer service = new ServiceLayerFileImpl(dao);
+        //GmsController controller = new GmsController(view, service);
+        //controller.execute();
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        GmsController control = ctx.getBean("control", GmsController.class);
+        control.execute();
     }
 }
